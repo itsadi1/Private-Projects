1600b81b25cb84320954b383ffd70ed926096b2e
Aditya Bajaj <93042011+itsadi1@users.noreply.github.com>
2022-02-26 00:57:38 +0530

    Delete test1.py

--- test1.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(host='localhost',user='admin',password='#admin',database='dbs')
-cursor = mydb.cursor()
-cursor.execute("SELECT Name FROM classxii")
-
-for x in mydb.cursor():
-    print(x)
